002b9ced7900167b4365790d3a84c7f6a26e20a4
MonishaChitral <81152653+MonishaChitral@users.noreply.github.com>
2021-03-22 15:04:36 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,26 @@
+try:
+	userinp = input("Enter input:")
+
+	if type(userinp) == int:
+		temp = userinp
+		rev  = 0
+		while(userinp>0):
+			dig=userinp%10
+			rev=(rev*10)+dig
+			userinp=userinp/10
+		if(temp == rev):
+			print("The number is palindrome!")
+		else:
+			print("The number is not palindrome!!")
+	else:
+		if userinp == userinp[::-1]:
+			print("The string is palindrome!")
+		else:
+			print("The string is not a palindrome!")
+		
+	print("Thank you:)")
+except EOFError as e:
+	print(e)	
+
+#import sys
+#print(sys.stdin)
